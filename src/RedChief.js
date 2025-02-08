68c98d8d01e08f14e5af28e60cdd4d4e07f5f06a
Ayushgupta208019 <116293093+Ayushgupta208019@users.noreply.github.com>
2025-02-09 00:36:08 +0530

    first commit

---
+++ src/RedChief.js
@@ -0,0 +1,5 @@
+function RedChief(){
+    return <h1>Red-RedChief the brand of branded shoes....</h1>
+}
+
+export default RedChief;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import RED from './RedChief';
+
+function Hello(){ //function component
+  return (
+    <h2>byee</h2>
+  )
+}
+
+
+//in React Variable called as State
+
+function RedWood(){
+  const [y, setY] = useState(20) //This is syntax for declare a varible in React. "State"
+
+  return <h1>Hello, The final value of y is {y}</h1>
+}
+
+let SalaryDetails = () =>{
+  const [salary, setSalary] = useState(50000)
+
+
+  let incr = () =>{
+    setSalary(salary + 10000)
+  }
+  return <div>
+    <h1>Salary is {salary}</h1>
+    <button onClick={() =>{ incr(); }}>Increment My Salary Now</button>
+  </div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Hello />
+    <RedWood />
+    <RED />
+    <SalaryDetails />
+  </React.StrictMode>
+);
